ff1a564634d982c45b4beaab46f1ced0990b4a9d
zhengzhengxiaogege <2265949083@qq.com>
2019-07-22 12:30:57 +0800

    add CornerButton.

--- app/src/main/java/com/github/xiaogegechen/fineviews/MainActivity.java
+++ app/src/main/java/com/github/xiaogegechen/fineviews/MainActivity.java
@@ -2,12 +2,28 @@ package com.github.xiaogegechen.fineviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.github.xiaogegechen.library.CornerButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private CornerButton mShareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        mShareButton = findViewById (R.id.share_button);
+        mShareButton.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Log.d (TAG, "onClick: ");
+                mShareButton.setIcon (R.drawable.change);
+            }
+        });
     }
 }
--- library/src/main/java/com/github/xiaogegechen/library/CornerButton.java
+++ library/src/main/java/com/github/xiaogegechen/library/CornerButton.java
@@ -2,6 +2,8 @@ package com.github.xiaogegechen.library;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
@@ -9,6 +11,7 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -43,6 +46,7 @@ public class CornerButton extends View {
 
     public CornerButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super (context, attrs, defStyleAttr);
+        // 属性值
         TypedArray a = context.obtainStyledAttributes (attrs, R.styleable.CornerButton);
         mStartColor = a.getColor (R.styleable.CornerButton_corner_button_start_color, getResources ().getColor (R.color.corner_button_start_color));
         mEndColor = a.getColor (R.styleable.CornerButton_corner_button_end_color, getResources ().getColor (R.color.corner_button_end_color));
@@ -54,6 +58,9 @@ public class CornerButton extends View {
         mIcon = a.getDrawable (R.styleable.CornerButton_corner_button_icon);
         mDistance = a.getDimensionPixelSize (R.styleable.CornerButton_corner_button_distance, DISTANCE_DEFAULT);
         a.recycle ();
+
+        // 可点击
+        setClickable (true);
 
         mTextPaint = new Paint ();
         mTextPaint.setTextSize (mTextSize);
@@ -92,39 +99,46 @@ public class CornerButton extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw (canvas);
+        // 宽高和内边距
         int width = getWidth ();
         int height = getHeight ();
+        int paddingLeft = getPaddingLeft ();
+        int paddingRight = getPaddingRight ();
+        int paddingTop = getPaddingTop ();
+        int paddingBottom = getPaddingBottom ();
+        int realWidth = width - paddingLeft - paddingRight;
+        int realHeight = height - paddingTop - paddingBottom;
         // (1)画背景
         if (mBgLinearGradient == null) {
-            mBgLinearGradient = new LinearGradient (0, 0, width, height, mStartColor, mEndColor, Shader.TileMode.REPEAT);
+            mBgLinearGradient = new LinearGradient (paddingLeft, paddingTop, paddingLeft + realWidth, paddingTop + realHeight, mStartColor, mEndColor, Shader.TileMode.REPEAT);
         }
         mBgPaint.setShader (mBgLinearGradient);
-        if(width <= height){
+        if(realWidth <= realHeight){
             // 背景是圆形
-            canvas.drawCircle ((float) (width * 1.0 / 2), (float) (width * 1.0 / 2), (float) (width * 1.0 / 2), mBgPaint);
+            canvas.drawCircle ((float) (realWidth * 1.0 / 2 + paddingLeft), (float) (realHeight * 1.0 / 2 + paddingTop), (float) (realWidth * 1.0 / 2), mBgPaint);
         } else {
             // 背景是圆角矩形
-            float r = (float) (height * 1.0 / 2);
-            mLeftRectF.set (0, 0, r * 2, r * 2);
-            mRightRectF.set (width - r * 2, 0, width, r * 2);
-            mBorderPath.moveTo (width - r , height);
-            mBorderPath.lineTo (r, height);
+            float r = (float) (realHeight * 1.0 / 2);
+            mLeftRectF.set (paddingLeft, paddingTop, paddingLeft + r * 2, paddingTop + r * 2);
+            mRightRectF.set (paddingLeft + realWidth - r * 2, paddingTop, paddingLeft + realWidth, paddingTop + r * 2);
+            mBorderPath.moveTo (paddingLeft + realWidth - r , paddingTop + realHeight);
+            mBorderPath.lineTo (paddingLeft + r, paddingTop + realHeight);
             mBorderPath.arcTo (mLeftRectF, 90, 180, false);
-            mBorderPath.lineTo (width - r , 0);
+            mBorderPath.lineTo (paddingLeft + realWidth - r , paddingTop);
             mBorderPath.arcTo (mRightRectF, 270, 180, false);
             canvas.drawPath (mBorderPath, mBgPaint);
         }
 
         // (2)画边框
         if(width <= height){
-            // 背景是圆形
-            canvas.drawCircle ((float) (width * 1.0 / 2), (float) (width * 1.0 / 2), (float) (width * 1.0 / 2), mBorderPaint);
+            // 边框是圆形
+            canvas.drawCircle ((float) (realWidth * 1.0 / 2 + paddingLeft), (float) (realHeight * 1.0 / 2 + paddingTop), (float) (realWidth * 1.0 / 2), mBorderPaint);
         } else {
-            // 背景是圆角矩形
+            // 边框是圆角矩形
             canvas.drawPath (mBorderPath, mBorderPaint);
         }
 
-        // (3)确定文字位置，文字位置,画文字
+        // (3)确定文字位置，画文字
         mTextPaint.getTextBounds (mText, 0, mText.length (), mTextBound);
         // 文字宽高
         int textHeight = mTextBound.height ();
@@ -139,17 +153,69 @@ public class CornerButton extends View {
         iconWidth = (int) (iconWidth * scale);
         // 内容宽
         int contentWidth = textWidth + iconWidth + mDistance;
-        float textPositionX = (float) ((width - contentWidth + textWidth) * 1.0 / 2);
-        float textPositionY = (float)(height * 1.0 / 2) - (mTextPaint.getFontMetrics ().top + mTextPaint.getFontMetrics ().bottom) / 2;
+        float textPositionX = (float) (paddingLeft + (realWidth - contentWidth + textWidth) * 1.0 / 2);
+        float textPositionY = paddingTop + (float)(realHeight * 1.0 / 2) - (mTextPaint.getFontMetrics ().top + mTextPaint.getFontMetrics ().bottom) / 2;
         canvas.drawText (mText, textPositionX, textPositionY, mTextPaint);
 
-        // (4)画图标
-        int iconLeft = (width - contentWidth) / 2 + textWidth + mDistance;
-        int iconTop = (height - iconHeight) / 2;
+        // (4)确定图标位置,画图标
+        int iconLeft = paddingLeft + (realWidth - contentWidth) / 2 + textWidth + mDistance;
+        int iconTop = paddingTop + (realHeight - iconHeight) / 2;
         int iconRight = iconLeft + iconWidth;
         int iconBottom = iconTop + iconHeight;
         mIcon.setBounds (iconLeft, iconTop, iconRight, iconBottom);
         canvas.save ();
         mIcon.draw (canvas);
+    }
+
+    // 动态设置属性
+    public void setStartColor(int startColor) {
+        mStartColor = startColor;
+        invalidate ();
+    }
+
+    public void setEndColor(int endColor){
+        mEndColor = endColor;
+        invalidate ();
+    }
+
+    public void setBorderColor(int borderColor) {
+        mBorderColor = borderColor;
+        invalidate ();
+    }
+
+    public void setTextColor(int textColor) {
+        mTextColor = textColor;
+        invalidate ();
+    }
+
+    public void setBorderWidth(int borderWidth) {
+        mBorderWidth = borderWidth;
+        invalidate ();
+    }
+
+    public void setText(String text) {
+        mText = text;
+        invalidate ();
+    }
+
+    public void setTextSize(int textSize) {
+        mTextSize = textSize;
+        invalidate ();
+    }
+
+    public void setIcon(Drawable icon) {
+        mIcon = icon;
+        invalidate ();
+    }
+
+    public void setIcon(int resourceId){
+        Bitmap bitmap = BitmapFactory.decodeResource (getResources (), resourceId);
+        mIcon = new BitmapDrawable (bitmap);
+        invalidate ();
+    }
+
+    public void setDistance(int distance) {
+        mDistance = distance;
+        invalidate ();
     }
 }
